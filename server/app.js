5cb721e9a44fad03c8d392f10ff17d5db918d549
Ayo <ayokrohn@gmail.com>
2022-06-19 13:22:07 +0200

    added socket to app.js and changed db file

--- server/app.js
+++ server/app.js
@@ -6,6 +6,14 @@ app.use(express.urlencoded({extended: true}));
 import path from "path";
 app.use(express.static(path.resolve("../client/public")));
 
+import session from "express-session";
+const sessionMiddleware = session({
+    secret: "heylove",//process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+});
+app.use(sessionMiddleware);
+
 import http from "http";
 const server = http.createServer(app);
 
@@ -13,17 +21,17 @@ import { Server } from "socket.io";
 const io = new Server(server);
 
 io.on("connection", (socket) => {
-    socket.on
+  console.log('A new client has connected!');
+  console.log(socket.id);
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
 })
 
-import session from "express-session";
-const sessionMiddelWare = session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true,
-});
+const wrap = middleware => (socket, next) => middleware(socket.request, {}, next);
+io.use(wrap(sessionMiddleware));
 
-app.use(sessionMiddelWare);
+app.get('*', (req, res) => res.sendFile(path.resolve('../client/public/index.html')));
 
 server.listen(3000, () => {
     console.log('listening on port 3000');
--- server/db/createConnection.js
+++ server/db/createConnection.js
@@ -1,8 +1,3 @@
-//mongodb+srv://admin:<password>@cluster0.zb5dh.mongodb.net/?retryWrites=true&w=majority
-
-import {MongoClient} from "mongodb";
-
-const url = "mongodb+srv://admin:<LearnMore2>@cluster0.zb5dh.mongodb.net/?retryWrites=true&w=majority";
 
 
 
